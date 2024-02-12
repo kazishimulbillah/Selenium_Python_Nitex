d05ac9e86130b34cb7f8c091fa1bb14f87f6f949
Kazi Md Shimul Billah <kazimdshimulbillah@Kazis-MacBook-Pro.local>
2024-02-12 23:15:45 +0600

    commit for nitex Assignment

---
+++ test_data/config.py
@@ -0,0 +1,28 @@
+class TestData:
+    FIREFOX_EXECUTABLE_PATH = "C:/browser_driver/geckodriver"
+    DEFAULT_BROWSER = "chrome"
+    USER_EMAIL = "cucumberbstest2022@gmalla.com"
+    PASSWORD = "testpass123@"
+
+
+    LOGIN_PAGE_URL = "http://automationpractice.com/index.php?controller=authentication&back=my-account"
+    LOGIN_PAGE_TITLE = "Login - My Store"
+    USER_ACCOUNT_PAGE_TITLE = "My account - My Store"
+    USER_ACCOUNT_NAME = "John Kanir"
+
+    FORMPAGE_URL = "https://demoqa.com/automation-practice-form"
+    FORMPAGE_TITLE = "ToolsQA"
+
+    FIRT_NAME = "ABDUR"
+    LAST_NAME = "RAHMAN"
+    EMAIL = "abdurTEst@gmtest.com"
+    GENDER = "Male"
+    MOBILE_NUMBER = "0123456789"
+    DAY = 12
+    DOB_MONTH = "February"
+    DOB_YEAR = 1998
+    HOBBIES = "Sports"
+    PICTURE = ''
+    CURRENT_ADDRESS = "Dhaka,BD"
+    CITY = "NCR"
+    STATE = "Delhi"
